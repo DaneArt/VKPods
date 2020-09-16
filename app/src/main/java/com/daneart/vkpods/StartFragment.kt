da1e9e090f34f60d7d0b1a34050eeb5676961fa5
2003l <2002.leonid2003@gmail.com>
2020-09-16 16:45:45 +0300

    LEO

---
+++ app/src/main/java/com/daneart/vkpods/StartFragment.kt
@@ -0,0 +1,31 @@
+package com.daneart.vkpods
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+
+class StartFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = StartFragment()
+    }
+
+    private lateinit var viewModel: StartViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.start_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(StartViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/daneart/vkpods/StartViewModel.kt
@@ -0,0 +1,7 @@
+package com.daneart.vkpods
+
+import androidx.lifecycle.ViewModel
+
+class StartViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
